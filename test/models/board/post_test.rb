e3617019983bba0ee7c4d11b62e9001217bc6f25
ka <ka.kaosf@gmail.com>
2015-07-02 00:05:24 +0900

    Add Board::Post factory test

--- test/models/board/post_test.rb
+++ test/models/board/post_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class Board::PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "factory" do
+    board_post = create :board_post
+    assert !board_post.id.nil?
+    assert board_post.valid?
+  end
 end
