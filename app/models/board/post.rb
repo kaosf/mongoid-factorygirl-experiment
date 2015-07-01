bf5f418e2caae05ba642c54460975017656c452c
ka <ka.kaosf@gmail.com>
2015-07-01 23:47:21 +0900

    Modify Board::Post to have many children and descendants

--- app/models/board/post.rb
+++ app/models/board/post.rb
@@ -2,4 +2,8 @@ class Board::Post
   include Mongoid::Document
   field :name, type: String
   field :text, type: String
+  belongs_to :parent,    class_name: 'Board::Post', inverse_of: :children
+  belongs_to :topic,     class_name: 'Board::Post', inverse_of: :descendants
+  has_many :children,    class_name: 'Board::Post', inverse_of: :parent
+  has_many :descendants, class_name: 'Board::Post', inverse_of: :topic
 end
